1e733681152ff75ee783cdbf1216f3da65ea512f
Abhishek Kumar <abhishekbgs075@gmail.com>
2025-12-19 11:17:13 +0530

    Add files via upload

---
+++ LARGEST_.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+  int a = 10, b = 20, c = 5;
+    if (a>b && a>c)
+       printf("a is largest");
+    else if(b>c)
+       printf("b is largest");
+    else
+       printf("c is largest");
+    return 0;
+}
